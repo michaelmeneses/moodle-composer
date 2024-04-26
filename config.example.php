9f69fce8bb2d9df028f3264ac7ee07a6e9f31d62
Michael Meneses <michael.douglas.meneses.2@gmail.com>
2024-04-26 15:04:35 -0300

    ADD support to .env

---
+++ config.example.php
@@ -0,0 +1,40 @@
+<?php // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+// Import moodlecomposer-util.php
+require_once(__DIR__ . '/../moodlecomposer-util.php');
+
+//=========================================================================
+// 1. DATABASE SETUP
+//=========================================================================
+$CFG->dbtype    = moodlecomposer_get_env('MOODLE_DBTYPE', 'mysqli');
+$CFG->dblibrary = moodlecomposer_get_env('MOODLE_DBLIBRARY', 'native');
+$CFG->dbhost    = moodlecomposer_get_env('MOODLE_DBHOST', 'localhost');
+$CFG->dbname    = moodlecomposer_get_env('MOODLE_DBNAME');
+$CFG->dbuser    = moodlecomposer_get_env('MOODLE_DBUSER');
+$CFG->dbpass    = moodlecomposer_get_env('MOODLE_DBPASS');
+$CFG->prefix    = moodlecomposer_get_env('MOODLE_DBPREFIX', 'mdl_');
+$CFG->dboptions = array(
+    'dbpersist' => 0,
+    'dbport' => moodlecomposer_get_env('MOODLE_DBPORT', ''),
+    'dbsocket' => '',
+    'dbcollation' => moodlecomposer_get_env('MOODLE_DBCOLLATION', 'utf8mb4_unicode_ci'),
+);
+
+//=========================================================================
+// 2. WEB SITE LOCATION
+//=========================================================================
+$CFG->wwwroot   = moodlecomposer_get_env('MOODLE_WWWROOT');
+
+//=========================================================================
+// 3. DATA FILES LOCATION
+//=========================================================================
+$CFG->dataroot  = moodlecomposer_get_env('MOODLE_DATAROOT');
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no PHP closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
